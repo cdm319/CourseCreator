293965a593939406b6d6008852329a6763c4a6bb
Chris Matthews <chris@cmcomputing.co.uk>
2014-05-05 15:08:32 +0100

    Added ability to create Units from scratch using form.  Added Unit Details page.  General bug fixes.

--- CourseCreator/src/uk/ac/lboro/coursecreator/model/Unit.java
+++ CourseCreator/src/uk/ac/lboro/coursecreator/model/Unit.java
@@ -4,16 +4,32 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
 /**
  * Unit model bean.
  * 
  * @author Chris Matthews <C.Matthews2-09@student.lboro.ac.uk>
  */
 public class Unit {
+	//the unique Unit ID number
 	private int unitId;
+	
+	//the title of the Unit
+	@Size(min=3, max=80, message="Please enter between 3 and 80 characters.")
 	private String title;
+	
+	//the date on which the Unit will be released
+	@NotEmpty(message="Please enter the Unit Release Date.")
 	private Date releaseDate;
-	private String nowAvailable;
+	
+	//whether the Unit is available now or not
+	private Boolean nowAvailable;
+	
+	//list of Lesson objects within the unit
 	private List<Lesson> lessons;
 	
 	//Basic constructor method
@@ -21,7 +37,7 @@ public class Unit {
 		this.unitId = 0;
 		this.title = "";
 		this.releaseDate = new Date(0);
-		this.nowAvailable = "N";
+		this.nowAvailable = false;
 		this.lessons = new ArrayList<Lesson>();
 	}
 	
@@ -44,10 +60,10 @@ public class Unit {
 	public void setReleaseDate(Date releaseDate) {
 		this.releaseDate = releaseDate;
 	}
-	public String getNowAvailable() {
+	public Boolean getNowAvailable() {
 		return nowAvailable;
 	}
-	public void setNowAvailable(String nowAvailable) {
+	public void setNowAvailable(Boolean nowAvailable) {
 		this.nowAvailable = nowAvailable;
 	}
 	public List<Lesson> getLessons() {
